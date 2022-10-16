bcadc6b0c4722c3ecaee3a10eef0114314850d50
ri88akka@gmail.com <ri88>
2022-10-16 21:45:39 +0700

    update static file

--- template/default/html/public/js/main.js
+++
@@ -1,9 +0,0 @@
-function myFunction(){
-    var x = document.getElementById("x");
-    if(x.style.display ==="none")
-    {
-        x.style.display = "block";
-    }else {
-        x.style.display = "none";
-    }
-}
