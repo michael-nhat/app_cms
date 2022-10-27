0a9aa86325acb00bcb347085583e951406d70d6d
ri88akka@gmail.com <ri88>
2022-10-27 21:34:38 +0700

    explore macs template

--- runtime/temp/39ecaeef4ddfa5152d0d447cea900b01.php
+++ runtime/temp/39ecaeef4ddfa5152d0d447cea900b01.php
@@ -1,4 +1,4 @@
-<?php if (!defined('THINK_PATH')) exit(); /*a:9:{s:38:"template/default/html/index/index.html";i:1666791342;s:70:"/www/wwwroot/maccms10test2/template/default/html/blocks/head-tags.html";i:1666791231;s:64:"/www/wwwroot/maccms10test2/template/default/html/blocks/top.html";i:1666679231;s:76:"/www/wwwroot/maccms10test2/template/default/html/blocks/popular--slides.html";i:1666366068;s:71:"/www/wwwroot/maccms10test2/template/default/html/blocks/new-movies.html";i:1666620061;s:70:"/www/wwwroot/maccms10test2/template/default/html/blocks/tv-series.html";i:1666623429;s:68:"/www/wwwroot/maccms10test2/template/default/html/blocks/top-ten.html";i:1666772514;s:67:"/www/wwwroot/maccms10test2/template/default/html/blocks/footer.html";i:1666774314;s:79:"/www/wwwroot/maccms10test2/template/default/html/blocks/script-bottom-body.html";i:1666791328;}*/ ?>
+<?php if (!defined('THINK_PATH')) exit(); /*a:9:{s:38:"template/default/html/index/index.html";i:1666875498;s:70:"/www/wwwroot/maccms10test2/template/default/html/blocks/head-tags.html";i:1666791231;s:64:"/www/wwwroot/maccms10test2/template/default/html/blocks/top.html";i:1666679231;s:76:"/www/wwwroot/maccms10test2/template/default/html/blocks/popular--slides.html";i:1666366068;s:71:"/www/wwwroot/maccms10test2/template/default/html/blocks/new-movies.html";i:1666620061;s:70:"/www/wwwroot/maccms10test2/template/default/html/blocks/tv-series.html";i:1666623429;s:68:"/www/wwwroot/maccms10test2/template/default/html/blocks/top-ten.html";i:1666772514;s:67:"/www/wwwroot/maccms10test2/template/default/html/blocks/footer.html";i:1666774314;s:79:"/www/wwwroot/maccms10test2/template/default/html/blocks/script-bottom-body.html";i:1666791328;}*/ ?>
 <!DOCTYPE html>
 <html>
   <head>
@@ -421,6 +421,16 @@
   </div>
    
         </div>
+        <div class="sui-right-wrap">
+
+
+
+          <!-- {maccms:type num="300"  tab="art"} -->
+          <?php $__TAG__ = '{"num":"36","paging":"yes","type":"current","order":"desc","by":"time","id":"vo","key":"key"}';$__LIST__ = model("Vod")->listCacheData($__TAG__);$__PAGING__ = mac_page_param($__LIST__['total'],$__LIST__['limit'],$__LIST__['page'],$__LIST__['pageurl'],$__LIST__['half']); if(is_array($__LIST__['list']) || $__LIST__['list'] instanceof \think\Collection || $__LIST__['list'] instanceof \think\Paginator): $key = 0; $__LIST__ = $__LIST__['list'];if( count($__LIST__)==0 ) : echo "" ;else: foreach($__LIST__ as $key=>$vo): $mod = ($key % 2 );++$key;          echo '<pre>';
+          var_dump($vo);
+          echo '</pre>';
+          endforeach; endif; else: echo "" ;endif; ?>
+        </div>
       </div>
       <div class="layui-col-lg2 sui-left-col">
         <div class="blk-top-ten">
@@ -636,7 +646,9 @@
 </div>
 
         <div class="hot-tags-blk">Hot tags block</div>
-        <div class="statictis-blk">Website statictis block</div>
+        <div class="statictis-blk">
+         
+        </div>
       </div>
       <div class="footer layui-col-lg12">footer
 
